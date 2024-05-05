1d4a787d580262b31ce2d6dbba721fec49592558
Erik Pöhler <vardumper@users.noreply.github.com>
2024-05-05 19:29:55 +0200

    v0.1.5 (#10)
    * chore: removed temp files
    
    * chore(deps): bump tar from 6.2.0 to 6.2.1 (#7)
    
    Bumps [tar](https://github.com/isaacs/node-tar) from 6.2.0 to 6.2.1.
    - [Release notes](https://github.com/isaacs/node-tar/releases)
    - [Changelog](https://github.com/isaacs/node-tar/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/isaacs/node-tar/compare/v6.2.0...v6.2.1)
    
    ---
    updated-dependencies:
    - dependency-name: tar
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore: deps updated
    
    * chore: deps updated
    
    * chore: upgrade deps (#8)
    
    Co-authored-by: Erik Pöhler <info@erikpoehler.com>
    
    * chore(deps): bump ejs in the npm_and_yarn group across 1 directory (#9)
    
    Bumps the npm_and_yarn group with 1 update in the / directory: [ejs](https://github.com/mde/ejs).
    
    
    Updates `ejs` from 3.1.9 to 3.1.10
    - [Release notes](https://github.com/mde/ejs/releases)
    - [Commits](https://github.com/mde/ejs/compare/v3.1.9...v3.1.10)
    
    ---
    updated-dependencies:
    - dependency-name: ejs
      dependency-type: indirect
      dependency-group: npm_and_yarn
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Erik Pöhler <info@erikpoehler.com>

--- src/Command/PicoCssSpecificationCommand.php
+++ src/Command/PicoCssSpecificationCommand.php
@@ -26,6 +26,46 @@ class PicoCssSpecificationCommand
         $htmlSpecs = Yaml::parseFile(getcwd() . Paths::HTML_SPECIFICATION_FILE);
 
         $specs = \array_merge_recursive($htmlSpecs, $picoSpecs);
+
+        /** @todo add generic attributes to every element (eg: tooltip, class button etc.) */
+
+        $specs = $htmlSpecs;
+
+        // global CSS attributes that can be used on _ANY_ element
+        if (array_key_exists('*', $picoSpecs)) {
+            foreach ($htmlSpecs as $element => $props) {
+                if (!isset($specs[$element]['attributes'])) {
+                    $specs[$element]['attributes'] = $picoSpecs['*']['attributes'];
+
+                    continue;
+                }
+                $specs[$element]['attributes'] = \array_merge_recursive($specs[$element]['attributes'], $picoSpecs['*']['attributes']);
+            }
+        }
+
+        unset($specs['*']);
+
+        // Regex matching
+        foreach (array_keys($picoSpecs) as $pattern) {
+            if (@preg_match($pattern, '') !== false) {
+                $keys = array_keys($specs);
+                $result = preg_grep($pattern, $keys);
+
+                foreach ($result as $key) {
+                    if (!isset($htmlSpecs[$key]['attributes'])) {
+                        $specs[$key]['attributes'] = $picoSpecs[$pattern]['attributes'];
+
+                        continue;
+                    }
+                    $specs[$key]['attributes'] = \array_merge_recursive($specs[$key]['attributes'], $picoSpecs[$pattern]['attributes']);
+                }
+
+                unset($picoSpecs[$pattern]); // remove regex key after processing (if there)
+            }
+        }
+
+        // deep merge everything else
+        $specs = \array_merge_recursive($specs, $picoSpecs);
         \file_put_contents(getcwd() . Paths::SPECIFICATION_FILE, Yaml::dump($specs, 10, 2));
 
         $io->success(sprintf('Generated Combined Specification to file %s', \basename(getcwd() . Paths::SPECIFICATION_FILE)));
