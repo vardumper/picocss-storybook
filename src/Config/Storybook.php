6891fe7866cd77798b1c46b839b32a3fa1bc2543
Erik Pöhler <erik.poehler@fork.de>
2024-09-07 13:01:08 +0200

    chore: updated yarn deps

---
+++ src/Config/Storybook.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace PicoCSS\Storybook\Config;
+
+class Storybook
+{
+    /**
+     *  Categories
+     */
+    public const CATEGORY_HTML = 'HTML5';
+    public const CATEGORY_TOKENS = 'Tokens';
+
+    /**
+     *  Folders
+     */
+    public const FOLDER_COLORS = 'Colors';
+}
