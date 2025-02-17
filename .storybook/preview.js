e0cead25683d97a0309118068f282807a5e78429
Erik Pöhler <erik.poehler@fork.de>
2025-02-17 14:58:24 +0100

    chore: upgrade to storybook@latest

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,6 +16,25 @@ import "../node_modules/@picocss/pico/css/pico.css"
 // import picocss from "./picocss";
 
 const preview = {
+  // decorators: [
+  //   // 👇 Defining the decorator in the preview file applies it to all stories
+  //   (Story, { parameters }) => {
+  //     // 👇 Make it configurable by reading from parameters
+  //     const { pageLayout } = parameters;
+  //     switch (pageLayout) {
+  //       // case 'page':
+  //       //   return `<div class="container-fluid">` + Story() + `</div>`;
+  //       // case 'page-mobile':
+  //       //   return Story();
+  //       default:
+  //         // In the default case, don't apply a layout
+  //         const container = document.getElementById('story-root');
+  //         // Story() returns the story's HTML string, so set that as the container's innerHTML:
+  //         return container.innerHTML = Story(); 
+  //         // return container.innerHTML; // Updated to return the innerHTML directly
+  //     }
+  //   },
+  // ],
   parameters: {
     controls: {
       expanded: true,
